569715193c927ae640675ab11d20f779634c0230
徐國堂 <roberthsu2003@gmail.com>
2023-12-14 13:49:55 +0000

    modify

--- lesson10_3.py
+++ lesson10_3.py
@@ -1,16 +1,15 @@
 import random
 import pyinputplus as pyip
 
-def getStudents() -> list[list]:
+def getStudents(student_nums:int=1, scores_nums:int=2) -> list[list]:
+    '''
+    參數: student_nums -> 學生人數\n
+    參數: scores_nums -> 科目數\n
+    '''
     with open('names.txt',mode='r',encoding='utf-8') as file:
         names:str = file.read()
 
     nameList:list[str] = names.split('\n')
-
-    student_nums:int = pyip.inputInt("請輸入學生的人數(1~50):",min=1,max=50)
-    scores_nums:int = pyip.inputInt("請輸入科目數(1~7):",min=1,max=7)
-
-
     students:list[list] = []
 
     names:list[str] = random.choices(nameList,k=student_nums)
@@ -26,5 +25,7 @@ def getStudents() -> list[list]:
 
 
 if __name__ == '__main__':
-    students:list[list] = getStudents()
+    s_nums:int = pyip.inputInt("請輸入學生的人數(1~50):",min=1,max=50)
+    o_nums:int = pyip.inputInt("請輸入科目數(1~7):",min=1,max=7)
+    students:list[list] = getStudents(student_nums=s_nums,scores_nums=o_nums)
     print(students)
