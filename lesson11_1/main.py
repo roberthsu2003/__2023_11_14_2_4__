ceea0e6e1aac170a2da86168ae2be6f169c76f16
徐國堂 <roberthsu2003@gmail.com>
2023-12-19 11:34:07 +0000

    modify

--- lesson11_1/main.py
+++ lesson11_1/main.py
@@ -31,4 +31,5 @@ if __name__ == '__main__':
     s_nums:int = pyip.inputInt("請輸入學生的人數(1~50):",min=1,max=50)
     o_nums:int = pyip.inputInt("請輸入科目數(1~7):",min=1,max=7)
     students:list[list] = getStudents(student_nums=s_nums,scores_nums=o_nums)
-    saveToCSV(fileName="abc",data=students)
+    fileName = pyip.inputFilename("請輸入檔案名稱(不用輸入副檔名稱):")
+    saveToCSV(fileName=fileName,data=students)
