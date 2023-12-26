ed3adadc3b3eab84825dc45b8e50a9525cb65f9d
roberthsu2003 <roberthsu2003@gmail.com>
2023-12-26 21:18:35 +0800

    modify

--- lesson13_1/main.py
+++ lesson13_1/main.py
@@ -6,5 +6,10 @@ if __name__ == "__main__":
     s_nums:int = pyip.inputInt("請輸入學生的人數(1~50):",min=1,max=50)
     students:list[dict] = getStudents(s_nums)
     fileName:str = pyip.inputFilename("請輸入檔案名稱(不用輸入副檔名稱):")
-    save_to_csv(students,fileName)
+    format = pyip.inputChoice(["1","2"],"請問要輸出那一個格式:\n按1 excel\n按2 csv\n請選擇:")
+    if format == "1":
+        print("儲存為excel檔")
+    else:
+        save_to_csv(students,fileName)
+    
     
